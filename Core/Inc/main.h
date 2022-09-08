f9c99dd55e2ab973d7b410e967b971163a962589
Super-Karl <67498728+Super-Karl@users.noreply.github.com>
2022-09-08 15:05:12 +0800

    实验2按键以及按键消抖

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -57,7 +57,7 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define K1_Pin GPIO_PIN_2
+#define K1_Pin GPIO_PIN_3
 #define K1_GPIO_Port GPIOE
 #define D0_Pin GPIO_PIN_0
 #define D0_GPIO_Port GPIOC
